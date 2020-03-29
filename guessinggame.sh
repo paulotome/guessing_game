645cc924486b36477cd010ebf82b894864279de4
Paulo Tomé <paulo.jorge.tome@gmail.com>
2020-03-29 21:57:41 +0100

    Reformulated guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/env bash
 # File: guessinggame.sh
 
-count=$(\ls -l . | grep -Ec '^-')
-
-function correct_guess {
-    if [[ $1 -lt $2 ]]
-    then
-	echo "Too low!"
-    elif [[ $1 -gt $2 ]]
-    then
-	echo "Too high!"
-    else
-	echo ""
-	echo "Congratulations! You guessed right!"
-    fi
+count_files () {
+    count=$(find . -maxdepth 1 -type f | wc -l)
 }
 
-while [[ $count -ne $guess ]]
+count_files
+
+read -rp "How many files are there in the current directory? " guess
+
+until [ "$count" -eq "$guess" ];
 do
-    read -p "How many files are there in the current directory? " guess
-    echo $(correct_guess $guess $count)
-    echo ""
+    if [ "$count" -gt "$guess" ] ;
+    then
+	read -rp "Your guess is too low! Try again " guess
+    else
+	read -rp "Your guess is too high! Try again " guess
+    fi
 done
+
+echo "Congratulations!! Your guess is correct!"
+
+exit 0
