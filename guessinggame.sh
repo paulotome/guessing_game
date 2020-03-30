ecf8a888bfffadb7957ad98384f56ed2f75a8a00
Paulo Tomé <paulo.tome@siscog.pt>
2020-03-30 19:38:16 +0100

    Fix counting of files in current folder. Renamad Makefile -> makefile

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,14 +1,9 @@
 #!/usr/bin/env bash
-# File: guessinggame.sh
-
 count_files () {
-    count=$(find . -maxdepth 1 -type f | wc -l)
+    count=$(find . -maxdepth 1 ! -path . | wc -l)
 }
-
 count_files
-
 read -rp "How many files are there in the current directory? " guess
-
 until [ "$count" -eq "$guess" ];
 do
     if [ "$count" -gt "$guess" ] ;
@@ -18,7 +13,5 @@ do
 	read -rp "Your guess is too high! Try again " guess
     fi
 done
-
 echo "Congratulations!! Your guess is correct!"
-
 exit 0
