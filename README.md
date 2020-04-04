00dbbc41c11261de862ab6ea2dad2174f7c09754
Paulo Tomé <paulo.jorge.tome@gmail.com>
2020-04-04 21:07:33 +0100

    Cope with "The program should not end until the user has entered the correct number of files in the current directory" requirement.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 <center> <h3>guessinggame.sh for Coursera/John Hopkins Univerity/The Unix Workbench course</h3> </center>
-README.md created at: Sat 04 Apr 2020 06:48:16 PM WEST
+README.md created at: Sat 04 Apr 2020 09:06:09 PM WEST
 
-Number of lines in guessinggame.sh: 32
+Number of lines in guessinggame.sh: 39
--- guessinggame.sh
+++ guessinggame.sh
@@ -3,29 +3,36 @@ count_files () {
     count=$(find . -maxdepth 1 ! -path . | wc -l)
 }
 count_files
+
 read -rp "How many files are there in the current directory? " guess
-if ! [[ "$guess" =~ ^[0-9]+$ ]]
-then
-    echo "Sorry integers only"
-    exit 1
-fi
+until [[ "$guess" =~ ^[0-9]+$ ]]
+do
+    if ! [[ "$guess" =~ ^[0-9]+$ ]]
+    then
+	read -rp "A integer must be supplied! How many files are there in the current directory? " guess
+    fi
+done
 until [ "$count" -eq "$guess" ];
 do
     if [ "$count" -gt "$guess" ]
     then
 	read -rp "Your guess is too low! Try again " guess
-	if ! [[ "$guess" =~ ^[0-9]+$ ]]
-	then
-	    echo "Sorry integers only"
-	    exit 1
-	fi
+	until [[ "$guess" =~ ^[0-9]+$ ]]
+	do
+	    if ! [[ "$guess" =~ ^[0-9]+$ ]]
+	    then
+		read -rp "A integer must be supplied! Try again " guess
+	    fi
+	done
     else
 	read -rp "Your guess is too high! Try again " guess
-	if ! [[ "$guess" =~ ^[0-9]+$ ]]
-	then
-	    echo "Sorry integers only"
-	    exit 1
-	fi
+	until [[ "$guess" =~ ^[0-9]+$ ]]
+	do
+	    if ! [[ "$guess" =~ ^[0-9]+$ ]]
+	    then
+		read -rp "A integer must be supplied! Try again " guess
+	    fi
+	done
     fi
 done
 echo "Congratulations!! Your guess is correct!"
